8ec476778fc2edae7f32c655b4faa2a0d6910db7
derek chen <derek.g.chen@gmail.com>
2018-03-23 17:08:53 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -17,7 +17,11 @@ def first_challenge
   contacts["Jon Snow"][:favorite_icecream_flavors] << "mint chip"
   contacts["Jon Snow"][:address] = "The Lord Commander's Rooms, The Wall, Westeros"
 
-
+  contacts["Freddy Mercury"].each do |attribute, value|
+    if attribute == :favorite_icecream_flavors
+      value.delete_if("strawberry")
+    end
+  end
 
 
   #remember to return your newly altered contacts hash!
